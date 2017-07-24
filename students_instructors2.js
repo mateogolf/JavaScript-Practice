4efff40d1ffd2472b7d0f15675f7987d3c67051f
mateogolf <rodriguezml11@mail.wlu.edu>
2017-07-24 16:48:44 -0400

    JS Practice Assignment 9: LAST
    Start counting at 1 instead of 0 for output

--- students_instructors2.js
+++ students_instructors2.js
@@ -1,6 +1,6 @@
 function printNames(arr){
     for(var i=0;i<arr.length;i++){
-        console.log(i + " - " + arr[i].first_name +
+        console.log((i+1) + " - " + arr[i].first_name +
             " " + arr[i].last_name  + " - " + 
             (arr[i].first_name.length + arr[i].last_name.length));
     }
