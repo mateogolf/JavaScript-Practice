5050b0cd33c6bba500d5bd6ce61c89a39538e877
mateogolf <rodriguezml11@mail.wlu.edu>
2017-07-23 20:21:52 -0400

    First JS assignment for coding dojo

---
+++ declare.js
@@ -0,0 +1,23 @@
+var num1 = 500;
+var num2 = -3;
+var num3 = 0;
+var num4 = 55001;
+
+var string1 = "Coding";
+var string2 = "Dojo";
+var string3 = "Matt";
+var bool1 = true;
+var bool2 = false;
+var temp = "";
+
+console.log("The First Number: ",num1);
+console.log("The Second Number: ",num2);
+console.log("The Third Number: ",num3);
+console.log("The Fourth Number: ",num4);
+
+console.log("The First String: ",string1);
+console.log("The Second String: ",string2);
+console.log("The Third String: ",string3);
+console.log("The First Boolean: ",bool1);
+console.log("The Second Boolean: ",bool2);
+console.log("Undefined: ",temp);
