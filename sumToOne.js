6d395a77e892715e3c6aa07a6beb963a4077de7c
mateogolf <rodriguezml11@mail.wlu.edu>
2017-07-24 11:55:42 -0400

    Week 2 Day 1 Algo Practice
    sumToOne function that add the digits in from an inputted number.  It repeats until there is only one digit. Example: sumToOne(928); returns 1 because 9+2+8=19->1+9=10->1+0=1

---
+++ sumToOne.js
@@ -0,0 +1,21 @@
+function sumToOne(num){
+    if(num<10){return num;}
+    var result = 0;
+    while(result == 0 || result > 10){
+        while(num>0){
+            result += num % 10;
+            num = Math.floor(num/10);
+        }
+        console.log("First result is:" + result);
+        if(result>=10){
+            num = result;
+            result = 0;
+        }
+    }
+    return result;
+}
+
+console.log(sumToOne(928));
+console.log(sumToOne(5000));
+console.log(sumToOne(5));
+console.log(sumToOne(5371));
