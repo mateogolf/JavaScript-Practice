edcce25317e4eb1d69f149befe15d74de72915a1
mateogolf <rodriguezml11@mail.wlu.edu>
2017-07-24 12:48:00 -0400

    JS Practice Assignment 7: Bonus included

---
+++ numbers_only.js
@@ -0,0 +1,11 @@
+function numbersOnly(arr){
+    var result = [];
+    for(var i=0;i<arr.length;i++){
+        if(typeof arr[i] === "number"){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+var newArray = numbersOnly([1, "apple", -3, "orange", 0.5]);
+console.log(newArray);
