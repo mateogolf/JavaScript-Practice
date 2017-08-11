ab22a4835a0782cf40dae24891c263dac9738f73
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-11 11:39:36 -0400

    Daily Algo - first node with some bonus functions

--- firstNode.js
+++ firstNode.js
@@ -1,45 +1,76 @@
-function Node(val){
+function Node(val) {
     this.val = val;
     this.null = null;
 }
 
-function SLL(){
+function SLL() {
     //attributes of SLL class
     this.null
-    this.insert = function(value){
-        if(!this.head){
+    this.insert = function (value) {
+        if (!this.head) { //Adds to beginning if nothing in list
             this.head = new Node(value);
             return this.head;
         }
+        //Traversal
         var current = this.head;
-        while(current.next){
+        while (current.next) {
             current = current.next;
         }
         current.next = new Node(value);
         return this.head;
     }
     //removing an item in the list - assuming there is only one node with that value
-    this.removeNode = function(value){
+    this.removeNode = function (value) {
         //find node's value
         var previous = this.head;
         var current = this.head;
-        var next = this.head;
-        while (current.val == value) {
+        if (!!current.next) {
+            var next = current.next;
+        }
+        else {
+            var next = null;
+        }
+        while (!!current.next) {
             //previous is old current, reset current to the next value, the new current's next value is assigned to next
             previous = current;
             current = current.next;
-            next = current.next;            
+            if (!!current.next) {
+                next = current.next;
+            }
+            else {
+                next = null;
+            }
         }//current is now the value to be removed
-        previous.next = next;
+        if (!!next) {
+            previous.next = next;
+        }
+        else {
+            previous.next = null;
+        }
+        return this;
+    }
+    //Print all in list
+    this.printAll = function (){
+        var current = this.head;
+        var count = 1;
+        console.log(count, ": ", current.val);
+        while (current.next) {
+            current = current.next;
+            count++;
+            console.log(count, ": ", current.val);
+        }
+        return this;
     }
 }
-// var node1 = new Node(100);
-// var node1 = new Node(101);
-// var node1 = new Node(102);
 var sL1 = new SLL();
 sL1.insert(100);
 sL1.insert(101);
 sL1.insert(102);
-console.log(sL1.head.val)//100
-console.log(sL1.head.next.val)//101
-console.log(sL1.head.next.next.val)//102
+sL1.printAll();
+sL1.removeNode(101)
+sL1.printAll();
+sL1.insert(102);
+sL1.insert(103);
+sL1.insert(104);
+sL1.insert(105);
+sL1.printAll();
