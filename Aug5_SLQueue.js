9ff923250a99d9f92e5b1f036c56c9cccf83b1f5
mateogolf <rodriguezml11@mail.wlu.edu>
2017-09-06 10:16:59 -0400

    Sep5 Daily Algo: SLQueues-enqueue and dequeue methods

---
+++ Aug5_SLQueue.js
@@ -0,0 +1,31 @@
+function Node(value){
+    this.val = value;
+    this.next = null;
+}
+function SLQueue(){
+    this.head = null;
+    this.tail = null;
+
+    this.enqueue = function(val){
+        if(!this.head){
+            this.head = new Node(val);
+            this.tail = this.head;
+            return this;
+        }
+        this.tail.next = new Node(val);
+        this.tail = this.tail.next;
+    }
+    this.dequeue = function () {//remove node from front and return val
+        if(!this.head){
+            return this.head;
+        }
+        if (!this.head.next) {
+            this.head = new Node(val);
+            this.tail = this.head;
+            return this;
+        }
+        var temp = this.head.val;
+        this.head = this.head.next;
+        return temp;        
+    }
+}
