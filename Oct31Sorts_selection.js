13ba88d5eed5272e09f2a1e7885d35a2f459c139
mateogolf <rodriguezml11@mail.wlu.edu>
2017-10-31 11:16:27 -0400

    Oct31Algo:working selection sort, using recursion

--- Oct31Sorts_selection.js
+++ Oct31Sorts_selection.js
@@ -1,39 +1,3 @@
-function bubbleSort(arr,k=0){
-    if (arr.length < 2) return arr;
-    let sorted = false;
-    for (let i = 0; i < arr.length-k; i++) {
-        if (arr[i] > arr[i + 1]) {
-            let temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i + 1]=temp;
-            sorted=true;
-        }
-        // console.log(`POS:${i}`, arr)
-    }
-    return sorted?bubbleSort(arr,k+1):arr;
-}
-// let array1 = [2, 3, -4, 10, 4, 0]
-// bubbleSort(array1)
-// console.log(array1);
-
-function insertionSort(arr){
-    if(arr.length<2) return arr;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>arr[i+1]){
-            for (let k = i+1; k>0; k--) {
-                if(arr[k]<arr[k-1]){
-                    let temp=arr[k];
-                    arr[k]=arr[k-1];
-                    arr[k-1]=temp;
-                }
-            }
-        }
-    }
-    return arr;
-}
-// let array2 = [2, 3, -4, 10, 4]
-// insertionSort(array2);
-// console.log(array2);
 function selectionSort(arr,current=0){
     if(current>=arr.length-1) return arr;
     //find min
