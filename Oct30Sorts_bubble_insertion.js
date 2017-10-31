9788d7e5450043ed4ae9a74e437ecf4b804bb519
mateogolf <rodriguezml11@mail.wlu.edu>
2017-10-31 11:13:54 -0400

    Oct31Algo:optimized bubbleSort from yesterday

--- Oct30Sorts_bubble_insertion.js
+++ Oct30Sorts_bubble_insertion.js
@@ -1,15 +1,16 @@
-function bubbleSort(arr){
+function bubbleSort(arr,k=0){
     if (arr.length < 2) return arr;
     let sorted = false;
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length-k; i++) {
         if (arr[i] > arr[i + 1]) {
             let temp = arr[i];
             arr[i] = arr[i+1];
             arr[i + 1]=temp;
             sorted=true;
         }
+        // console.log(`POS:${i}`, arr)
     }
-    return sorted?bubbleSort(arr):arr;
+    return sorted?bubbleSort(arr,k+1):arr;
 }
 let array1 = [2, 3, -4, 10, 4, 0]
 bubbleSort(array1)
